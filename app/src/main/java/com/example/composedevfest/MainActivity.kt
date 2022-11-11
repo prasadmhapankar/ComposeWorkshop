c64e070979324e68c60c1665d2face21912bc407
Prasad Mhapankar <prasadmhapankar91@gmail.com>
2022-11-11 18:35:36 +0000

    Image card

--- app/src/main/java/com/example/composedevfest/MainActivity.kt
+++ app/src/main/java/com/example/composedevfest/MainActivity.kt
@@ -3,59 +3,104 @@ package com.example.composedevfest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composedevfest.ui.theme.ComposeDevfestTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Greeting("Android", "Jetpack Compose")
+            ImageCard(
+                painter = painterResource(id = R.drawable.android_jetpack_header),
+                contentDescription = stringResource(id = R.string.content_description),
+                title = "Alfred",
+                description = "Android Developer",
+                //modifier = Modifier.padding(5.dp)
+            )
         }
     }
 }
 
 @Composable
-fun Greeting(text1: String, text2: String) {
-    Row(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.Yellow),
-        horizontalArrangement = Arrangement.SpaceBetween,
-        verticalAlignment = Alignment.CenterVertically,
+fun ImageCard(
+    painter: Painter,
+    contentDescription: String,
+    title: String,
+    description: String,
+    modifier: Modifier = Modifier,
+) {
+    Card(
+        modifier = modifier
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(size = 15.dp),
+        elevation = 4.dp,
     ) {
-        Text(text = "Hello $text1!")
-        Text(text = "Hello world!")
-        Text(text = "Hello $text2!")
+        Row(
+            modifier = Modifier
+                .height(100.dp)
+                .background(Color.White)
+                .padding(all = 16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                modifier = Modifier
+                    .height(80.dp)
+                    .width(80.dp),
+                painter = painter,
+                contentDescription = contentDescription,
+                contentScale = ContentScale.Crop,
+            )
+            Column(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .padding(10.dp),
+                verticalArrangement = Arrangement.Center,
+            ) {
+                Text(
+                    text = title,
+                    style = TextStyle(
+                        color = Color.Black,
+                        fontSize = 16.sp
+                    ),
+                )
+                Text(
+                    text = description,
+                    style = TextStyle(
+                        color = Color.DarkGray,
+                        fontSize = 14.sp
+                    ),
+                )
+            }
+        }
     }
-
-    /*Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.Yellow),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.SpaceAround,
-    ) {
-        Text(text = "Hello $text1!")
-        Text(text = "Hello world!")
-        Text(text = "Hello $text2!")
-    }*/
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeDevfestTheme {
-        Greeting("Android", "Jetpack Compose")
+        ImageCard(
+            painter = painterResource(id = R.drawable.ic_launcher_foreground),
+            contentDescription = stringResource(id = R.string.content_description),
+            title = "Alfred",
+            description = "Android Developer",
+        )
     }
 }
